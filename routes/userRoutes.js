7cb0a032b9b87827f09e9dcee9d5c76e7d3b9be2
mahmoud osman <devmaos@gmail.com>
2026-02-16 17:07:41 +0200

    add user CRUD routes with auth and role-based middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,28 @@
-import express from "express"
-import { createUser } from "../controllers/userController.js"
-const userRoute = express.Router()
+import express from "express";
+import {
+    createUser,
+    login,
+    getAllUsers,
+    getSingleUser,
+    deleteUser,
+    updateUser,
+    deleteAllUser,
+} from "../controllers/userController.js";
+import verifyToken from "../middleWare/verifyToken.js";
+import allowTo from "../middleWare/allowTo.js";
+const userRoute = express.Router();
 
 userRoute.post("/register", createUser);
+userRoute.post("/login", login);
 
-export  default userRoute
+userRoute
+    .route("/:id")
+    .get(verifyToken, allowTo(["admin", "moderator", "user"]), getSingleUser)
+    .delete(verifyToken, allowTo(["admin", "moderator"]), deleteUser)
+    .patch(verifyToken, allowTo(["admin", "moderator"]), updateUser);
+userRoute
+    .route("/")
+    .get(verifyToken, allowTo(["admin", "moderator", "user"]), getAllUsers)
+    .delete(verifyToken, allowTo(["superAdmin"]), deleteAllUser);
+
+export default userRoute;
