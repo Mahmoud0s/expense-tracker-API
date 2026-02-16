b34d30fb9eaec927af714ae54c2303e43598c30a
mahmoud osman <devmaos@gmail.com>
2026-02-16 17:05:01 +0200

    update User schema with roles, password hashing, and delete protection

--- model/userModel.js
+++ model/userModel.js
@@ -1,25 +1,53 @@
 import mongoose from "mongoose";
 import validator from "validator";
-const UserSchema=mongoose.Schema({
-    name:{
-        type:String,
-        require:true,
+import bcrypt from "bcrypt";
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        require:true,
-        vaildate:[validator.isEmail,"enter vaild email"]
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "enter vaild email"],
     },
-    userName:{
-        type:String,
-        require:true,
-        vaildate:[(value)=>validator.isLength(value,{min:6}),"user name must be at least 6 letters"]
+    userName: {
+        unique: true,
+        type: String,
+        required: true,
+        validate: [
+            (value) => validator.isLength(value, { min: 6 }),
+            "user name must be at least 6 letters",
+        ],
     },
-    password:{
-        type:String,
-        require:true,
-        validate:[(value)=>validator.isLength(value,{min:8}),"password must be more than 8 letters"]
-    }
+    password: {
+        type: String,
+        required: true,
+        validate: [
+            (value) => validator.isLength(value, { min: 8 }),
+            "password must be more than 8 letters",
+        ],
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["user", "moderator", "admin", "superAdmin"],
+        default: "user",
+    },
 });
-
-export default mongoose.model("user",UserSchema)
+UserSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
+});
+UserSchema.pre("deleteOne", async function () {
+    const checkDeletedUser = await this.model.findOne({
+        _id: this.getFilter(),
+    });
+    if (["admin", "superAdmin"].includes(checkDeletedUser.role))
+        throw new Error("cannot delete Admin or superAdmin 😑");
+});
+UserSchema.pre("findOneAndUpdate", async function () { // will change soon
+    // const updatedData = this.getUpdate(); 
+    // // if(updatedData.password)
+    // //     updatedData.password = await bcrypt.hash(updatedData.password, await bcrypt.genSalt(10));
+});
+export default mongoose.model("user", UserSchema);
