0c85e5f57acf13e371fc5547a99af1e874500a9d
mahmoud osman <devmaos@gmail.com>
2026-02-18 11:00:59 +0200

    add category routes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import errorMiddleWare from "./middleWare/errorMiddleWare.js";
 import dns from "dns";
 import "dotenv/config";
 
 dns.setServers(["8.8.8.8"]);
 
-mongoose.connect(
+mongoose
+    .connect(
         `mongodb+srv://${process.env.UserNameDB}:${process.env.PasswordDB}@${process.env.ClusterDB}.kdtuexs.mongodb.net/`,
-        { autoIndex: true }
+        { autoIndex: true },
     )
     .then(() => {
         console.log("conncted to DB");
@@ -19,10 +21,13 @@ const app = express();
 app.use(express.json());
 
 app.use("/user", userRoute);
-app.all("/*splat",(req,res,next)=>{
-    res.status(404).send({state:"error",message:`there is no url called:${req.url}`})
-    
-})
+app.use("/category", categoryRoutes);
+app.all("/*splat", (req, res, next) => {
+    res.status(404).send({
+        state: "error",
+        message: `there is no url called:${req.url}`,
+    });
+});
 
 app.use(errorMiddleWare);
 app.listen(process.env.port, () => {
