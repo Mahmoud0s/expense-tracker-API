366951cf09791fcb361d50c5905b721a84f98ed0
mahmoud osman <devmaos@gmail.com>
2026-02-26 22:44:55 +0200

    adding expenseRoutes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
 import errorMiddleWare from "./middleWare/errorMiddleWare.js";
 import dns from "dns";
 import "dotenv/config";
@@ -22,6 +23,7 @@ app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/category", categoryRoutes);
+app.use("/expense", expenseRoutes);
 app.all("/*splat", (req, res, next) => {
     res.status(404).send({
         state: "error",
